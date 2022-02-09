1ed83267e39b9c80621111729501526ddb6536ff
Kevin Jin <kjin@mednet.ucla.edu>
2022-02-09 20:51:08 +0000

    added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/ucla-kevinjin/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "ucla-kevinjin-workshop"
+  resource_group_name = "${var.prefix}-workshop"
   # insert required variables here
 }
